a0a56f4ce4a55c45441bb7b474dfc6511d7f5780
gfilangeri <gfilange@calpoly.edu>
2019-10-19 13:12:38 -0700

    init

---
+++ src/App.vue
@@ -0,0 +1,47 @@
+<template>
+    <div class="page-container">
+        <div>
+            hello! this is from the frontend
+        </div>
+        <div>
+            {{test}}
+        </div>
+    </div>
+</template>
+
+<script>
+    import axios from "axios";
+
+
+    export default {
+        name: "App",
+        data: () => ({
+            test: ""
+        }),
+        methods: {
+
+            getTest() {
+                axios
+                    .get("http://localhost:8080/")
+                    .then((response) => {
+                        this.test = response.data;
+                        console.log("success");
+                        this.$emit("success");
+                        console.log(this.test)
+                    })
+                    .catch(() => {
+                        console.log("catch");
+                    });
+            },
+        },
+        created() {
+            this.getTest()
+
+        }
+    };
+</script>
+
+<style lang="scss" scoped>
+
+</style>
+
